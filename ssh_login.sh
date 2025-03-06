9541e1a494b189374354f5078879ff60e14b2a79
lswmxl <lswm@140504.xyz>
2025-03-06 23:53:04 +0800

    更新 ssh_login.sh

--- ssh_login.sh
+++ ssh_login.sh
@@ -19,7 +19,7 @@ fi
 # 获取 SSH 登录信息
 USER=$(whoami)
 HOSTNAME=$(hostname)
-IP_ADDRESS=$(curl -s https://api.ipify.org)  # 获取公网 IP
+IP_ADDRESS=$(hostname -I | awk '{print $1}')  # 服务器内网 IP
 SSH_CLIENT_IP=$(echo $SSH_CONNECTION | awk '{print $1}')  # 登录 IP
 
 if [ -z "$SSH_CLIENT_IP" ]; then
@@ -38,7 +38,7 @@ LOCATION=$(curl -s "https://ipinfo.io/$SSH_CLIENT_IP/json" | \
 SERVER_PUBLIC_IP=$(curl -s https://api.ipify.org)  # 使用 ipify 获取公网 IP
 
 # 生成消息内容
-MESSAGE="🔔 SSH 登录通知%0A👤 用户: $USER%0A🖥 服务器: $HOSTNAME%0A🌐 服务器IP:$IP_ADDRESS%0A🌐 服务器IP: $SERVER_PUBLIC_IP%0A📡 登录IP: $SSH_CLIENT_IP%0A📍 位置: $LOCATION"
+MESSAGE="🔔 SSH 登录通知%0A👤 用户: $USER%0A🖥 服务器: $HOSTNAME%0A🌐 内网IP:$IP_ADDRESS%0A🌐 公网IP: $SERVER_PUBLIC_IP%0A📡 登录IP: $SSH_CLIENT_IP%0A📍 位置: $LOCATION"
 # 发送到 Telegram
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE" >/dev/null 2>&1 &
